6885e88b74ed743f98b3b4652583a946c78138a3
MUKILTEO\1707344 <1707344@mukilteo.wednet.edu>
2021-11-17 09:47:28 -0800

    Growing Kinda works. Will stop working once we get a not round sprite but we will see

---
+++ Assets/Gavin/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    Camera camera;
+    public float cameraSize;
+    [SerializeField] float cameraGrowthSpeed;
+    float lerpProgress;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
+        if (camera.orthographicSize < cameraSize)
+        {
+            camera.orthographicSize += Mathf.Lerp(cameraSize, camera.orthographicSize, cameraGrowthSpeed);
+        }
+        
+        if(camera.orthographicSize > cameraSize)
+        {
+            camera.orthographicSize = cameraSize;
+        }
+
+    }
+}

cbb4deaa8986c97c292bb5d1bccbbd8cf3321ac1
MUKILTEO\1707235 <1707235@mukilteo.wednet.edu>
2021-11-18 10:14:05 -0800

    Fixed error.

--- Assets/Thomas/Pause/Pause.cs
+++ Assets/Thomas/Pause/Pause.cs
@@ -1,38 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
+    public GameObject pauseMenuUI;
     public static bool GameIsPaused = false;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            Debug.Log("Trying to do thing");
             if (GameIsPaused)
             {
                 Resume();
             }
             else
             {
-                Pause();
+                Paus();
             }
 
         }
        
     }
-    void Resume()
+   
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
-    void Pause()
+    public void Paus()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
 
+    public void quitgame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void loadmenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Menu");
+        SceneManager.LoadScene("Menu");
+    }
+   
 }

3611c57882a60c76f286cc0b7013fa2e03df3ccb
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2021-11-19 10:11:14 -0800

    Started enemy Hp

---
+++ Assets/Matve/Scripts/EnemyHP.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public bool instaKill;
+
+    public float maxHealth;
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (instaKill)
+        {
+              
+        }
+    }
+}

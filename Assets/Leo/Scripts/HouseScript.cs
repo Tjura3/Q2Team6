8da5d3a5f6280b66f5cb50057ed47f16bb952110
LAPTOP-V6KQUTFR\Leo <leo_1517@outlook.com>
2021-11-21 02:06:15 -0800

    Finished house mechanic

---
+++ Assets/Leo/Scripts/HouseScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseScript : MonoBehaviour
+{
+
+    public int hidingBeans = 0;
+    public GameObject beans;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            hidingBeans += 1;
+            //Debug.Log(hidingBeans);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            for (int i = 1; i <= hidingBeans; i++)
+            {
+                Instantiate(beans, new Vector2(transform.position.x + i, transform.position.y), beans.transform.rotation);
+            }
+
+            if (gameObject != null)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}

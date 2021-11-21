7ed1b375a2faf7f7ddf449a5eec10fe80e13a3c5
Taylor C <tcadd47@gmail.com>
2021-11-20 17:50:28 -0800

    Lots and lots of bean ani testing

---
+++ Assets/Taylor/Scripts/CopiedPlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopiedPlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    Rigidbody2D rb2;
+    SpriteRenderer sr;
+    Animator a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        a = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            a.SetBool("Running", false);
+        }
+        else
+        {
+            a.SetBool("Running", true);
+        }
+
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+}

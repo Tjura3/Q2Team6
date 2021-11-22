e66b482cc2d91e8ad968c858d99285d64fa6b494
LAPTOP-V6KQUTFR\Leo <leo_1517@outlook.com>
2021-11-21 19:24:22 -0800

    working on flipping enemy sprite

--- Assets/Leo/Scripts/EnemyGFX.cs
+++ Assets/Leo/Scripts/EnemyGFX.cs
@@ -5,18 +5,25 @@ using Pathfinding;
 
 public class EnemyGFX : MonoBehaviour
 {
+    Rigidbody2D rb;
 
-    public AIPath aiPAth;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
 
+    }
     // Update is called once per frame
     void Update()
     {
-        if(aiPAth.desiredVelocity.x >= 0.01f)
+
+        if (rb.velocity.x >= 0.01f)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
-        } else if (aiPAth.desiredVelocity.x <= -0.01f)
+            Debug.Log("Right");
+        } else if (rb.velocity.x <= -0.01f)
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
+            Debug.Log("Left");
         }
     }
 }

ab924f3f376ca548bb4d07a3e25eb9b3f986320f
Gavin <gavinjohnson2004@gmail.com>
2021-11-23 18:47:28 -0800

    Made tongue code work better. Doesn't feel as nice but just need to change values

--- Assets/Gavin/Scripts/ElasticTongue.cs
+++ Assets/Gavin/Scripts/ElasticTongue.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class ElasticTongue : MonoBehaviour
 {
-    Transform[] points;
-    Rigidbody2D[] pointsRigidbody2Ds;
+    List<Point> points;
     LineRenderer lineRenderer;
 
     //The distance before line becomes stretchy
     [SerializeField] float startDistance;
     [SerializeField] float strechStrength;
+    [SerializeField] float damperStrength;
     [SerializeField] float maxSpeed;
 
     //For generating the rope
     [SerializeField] int numberOfPoints;
     [SerializeField] float initialDistanceBetweenPoints;
     [SerializeField] GameObject pointPrefab;
-    [SerializeField] float shootOutDistance;
 
     [SerializeField] Camera camera;
     [SerializeField] float mouseMoveSpeed;
@@ -27,24 +26,24 @@ public class ElasticTongue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        points = new List<Point>();
+
         GeneratePoints();
 
-        points = transform.GetComponentsInChildren<Transform>();
-        //Fixing weird problem with empty child
-        Transform[] tempPoints = new Transform[points.Length - 1];
-        for (int i = 0; i < tempPoints.Length; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            tempPoints[i] = points[i + 1];
+            if (transform.GetChild(i).CompareTag("Point"))
+            {
+                points.Add(new Point(transform.GetChild(i).gameObject));
+            }
         }
-        points = tempPoints;
-
-        pointsRigidbody2Ds = transform.GetComponentsInChildren<Rigidbody2D>();
-        Debug.Log(pointsRigidbody2Ds.Length);
 
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = points.Length;
+        lineRenderer.positionCount = points.Count;
 
-        pointsRigidbody2Ds[pointsRigidbody2Ds.Length-1].bodyType = RigidbodyType2D.Kinematic;
+        Debug.Log(points.Count);
+
+        points[points.Count-1].rb.bodyType = RigidbodyType2D.Kinematic;
         //points[points.Length - 1].position = new Vector3(6, 0, 0);
     }
 
@@ -72,23 +71,18 @@ public class ElasticTongue : MonoBehaviour
             Vector3 mousePos = Input.mousePosition;
             mousePos = camera.ScreenToWorldPoint(mousePos);
 
-            Vector2 dir = (mousePos - points[0].position).normalized;
+            Vector2 dir = (mousePos - points[0].transform.position).normalized;
 
 
 
-            pointsRigidbody2Ds[0].AddForce(dir * mouseMoveSpeed);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            pointsRigidbody2Ds[0].AddForce(Vector2.left * shootOutDistance);
+            points[0].rb.AddForce(dir * mouseMoveSpeed);
         }
 
         UpdatePoints();
         
     }
 
-    void UpdatePoints()
+    /*void UpdatePoints()
     {
         pointsRigidbody2Ds[pointsRigidbody2Ds.Length - 1].MovePosition(playerT.position);
 
@@ -103,7 +97,8 @@ public class ElasticTongue : MonoBehaviour
                     distance = 0;
                 }
 
-                float pullForce = strechStrength * distance;
+                float pullForce = -strechStrength * distance - damperStrength * distance;
+                pullForce *= -1;
 
                 if (i != points.Length - 1)
                 {
@@ -128,7 +123,8 @@ public class ElasticTongue : MonoBehaviour
                     distance = 0;
                 }
 
-                float pullForce = strechStrength * distance;
+                float pullForce = -strechStrength * distance - damperStrength * distance;
+                pullForce *= -1;
 
                 if (i != 0)
                 {
@@ -145,16 +141,78 @@ public class ElasticTongue : MonoBehaviour
         }
 
         //pointsRigidbody2Ds[points.Length - 1].velocity = Vector3.zero;
-    }
+    }*/
 
+
+    void UpdatePoints()
+    {
+        points[points.Count - 1].rb.MovePosition(playerT.position);
+
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            if (i != 0)
+            {
+                //Calculate one behind
+                points[i].rb.AddForce((points[i - 1].transform.localPosition - points[i].transform.localPosition).normalized * CalculatePullForce(points[i - 1], points[i]));
+            }
+
+            if (i != points.Count-1)
+            {
+                //Calculate one in front
+                points[i].rb.AddForce((points[i + 1].transform.localPosition - points[i].transform.localPosition).normalized * CalculatePullForce(points[i + 1], points[i]));
+            }
+        }
+
+        points[points.Count - 1].velocity = Vector3.zero;
+    }
     void DrawLine()
     {
 
-        Vector3[] pointsPos = new Vector3[points.Length];
+        Vector3[] pointsPos = new Vector3[points.Count];
         for (int i = 0; i < pointsPos.Length; i++)
         {
-            pointsPos[i] = points[i].position;
+            pointsPos[i] = points[i].transform.position;
         }
         lineRenderer.SetPositions(pointsPos);
+    }
+
+    float CalculatePullForce(Point point1, Point point2)
+    {
+
+        //Get Distance
+        float distance = Vector2.Distance(point1.transform.localPosition, point2.transform.localPosition);
+        distance -= startDistance;
+
+        if (distance < 0)
+        {
+            distance = 0;
+        }
+
+        //Calculate pullForce
+        float pullForce = -strechStrength * distance - damperStrength * distance;
+        pullForce *= -1;
+
+        if (pullForce > maxSpeed)
+        {
+            pullForce = maxSpeed;
+        }
+
+        return pullForce;
+    }
+
+}
+
+class Point
+{
+    public Vector2 velocity;
+    public Transform transform;
+    public Rigidbody2D rb;
+    public GameObject gameObject;
+    public Point(GameObject gameObject)
+    {
+        transform = gameObject.transform;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
     }
 }

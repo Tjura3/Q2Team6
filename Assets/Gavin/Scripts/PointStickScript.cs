ea7c429484cd6d6ca68b260aad488174e3a017d3
MUKILTEO\1707344 <1707344@mukilteo.wednet.edu>
2021-12-03 10:12:06 -0800

    Made tongue sticky

---
+++ Assets/Gavin/Scripts/PointStickScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PointStickScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag.CompareTo("Enemy") == 0)
+        {
+            ConstraintSource constraintSource = new ConstraintSource();
+            constraintSource.sourceTransform = transform;
+            constraintSource.weight = 1;
+            collision.gameObject.AddComponent<ParentConstraint>();
+            ParentConstraint pc = collision.gameObject.GetComponent<ParentConstraint>();
+            pc.locked = true;
+
+            pc.AddSource(constraintSource);
+            pc.constraintActive = true;
+        }
+    }
+}

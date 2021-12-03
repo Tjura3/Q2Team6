323529f7bd47b73b744e393d3cb5988822c860e7
LAPTOP-V6KQUTFR\Leo <leo_1517@outlook.com>
2021-12-03 11:20:59 -0800

    did stuff with walls

--- Assets/Leo/Scripts/HouseScript.cs
+++ Assets/Leo/Scripts/HouseScript.cs
@@ -11,7 +11,7 @@ public class HouseScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.tag == "RunEnemy")
         {
             hidingBeans += 1;
             //Debug.Log(hidingBeans);

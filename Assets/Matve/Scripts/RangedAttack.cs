a0ad440771a62bb6e353ee2c2d88c37cef3ed104
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2021-12-08 10:05:42 -0800

    Daily push for da day

--- Assets/Matve/Scripts/RangedAttack.cs
+++ Assets/Matve/Scripts/RangedAttack.cs
@@ -27,6 +27,8 @@ public class RangedAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if (HS.isDead)
         {
             inRange = false;

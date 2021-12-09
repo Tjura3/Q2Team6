c33598a2fbf08c3f82ada1e9dce88e259ad6186a
MUKILTEO\1707235 <1707235@mukilteo.wednet.edu>
2021-12-09 10:20:36 -0800

    Circle

--- Assets/Thomas/B/LevelLoader.cs
+++ Assets/Thomas/B/LevelLoader.cs
@@ -6,23 +6,32 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
 
+    public LevelLoader Sussysixtynineing;
     public Animator transition;
     public float transitionTime = 1f;
 
     // Update is called once per frame
     void Update()
     {
+        
+
         if (Input.GetMouseButtonDown(0))
         {
+            
             LoadNextLevel();
         }
+    }
+    public void Wackystacks()
+    {
+        Sussysixtynineing.LoadNextLevel();
     }
 
 
     public void LoadNextLevel()
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
         
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+       
     }
 
     IEnumerator LoadLevel(int levelIndex)

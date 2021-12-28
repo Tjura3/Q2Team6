edf04bc96f2d851bcfe6ed4117148fcd12a8c5d7
Cadd47 <tcadd47@gmail.com>
2021-12-28 12:46:09 -0800

    Invinc frames work whenever gertting hit by a big boi. Invinc frames for some reason don't occur from the shooter guy

---
+++ Assets/Taylor/Scripts/InvincFrame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvincFrame : MonoBehaviour
+{
+    public Material[] material;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown("o"))
+        //{
+        //    rend.sharedMaterial = material[1];
+        //}
+        //else if (Input.GetKeyDown("p"))
+        //{
+        //    rend.sharedMaterial = material[0];
+        //}
+
+        if (gameObject.GetComponent<healthSystem>().invFrame == true)
+        {
+            rend.sharedMaterial = material[0];
+        }
+        else if (gameObject.GetComponent<healthSystem>().invFrame == false)
+        {
+            rend.sharedMaterial = material[1];
+        }
+    }
+}

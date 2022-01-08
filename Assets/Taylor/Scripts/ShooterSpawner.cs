e363b5121de98b0838dbf75bc3dece1cea2c491a
Cadd47 <tcadd47@gmail.com>
2022-01-08 14:08:41 -0800

    played around with spawners and set a global enemy max, along with making spawners work for bigbois and shooters

--- Assets/Taylor/Scripts/ShooterSpawner.cs
+++ Assets/Taylor/Scripts/ShooterSpawner.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class ShooterSpawner : MonoBehaviour
 {
     public GameObject[] enemies;
     public Transform[] spawnPoint;
 
     private int rand;
     private int randPosition;
-    public static int enemyNumber = 0;
-    public static int totalEnemies = 0;
+    public static int shooterEnemies = 0;
 
     public float startTimeBetweenSpawns;
     private float timeBetweenSpawns;
     // Start is called before the first frame update
-    
+
     private void Start()
     {
         timeBetweenSpawns = startTimeBetweenSpawns;
@@ -24,27 +23,21 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if (timeBetweenSpawns <= 0 && enemyNumber <= 29 && totalEnemies <= 199)
+        if (timeBetweenSpawns <= 0 && shooterEnemies <= 4)
         {
             rand = Random.Range(0, enemies.Length);
-            randPosition = Random.Range(0, spawnPoint.Length); 
+            randPosition = Random.Range(0, spawnPoint.Length);
 
             Instantiate(enemies[rand], spawnPoint[randPosition].transform.position, Quaternion.identity);
             timeBetweenSpawns = startTimeBetweenSpawns;
 
-            enemyNumber += 1;
-            totalEnemies += 1;
-            Debug.Log("Enemies:" + enemyNumber);
-            Debug.Log("Total Enemies:" + totalEnemies);
+            shooterEnemies += 1;
+            Spawner.totalEnemies += 1;
+            Debug.Log("Shooters:" + shooterEnemies);
         }
         else
         {
             timeBetweenSpawns -= Time.deltaTime;
-        }
-
-        if (Input.GetKeyDown("p"))
-        {
-            //enemyNumber -= 10;
         }
     }
 }

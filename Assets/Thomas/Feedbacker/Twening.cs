332de7725e32b0ccb6913636f3c4f9aa5a7f6ba0
Thomas <1707235@mukilteo.wednet.edu>
2022-01-09 18:36:59 -0800

    fixed transition error, and finished player feedback. it is now a prefab. Bbar under PlayerFeedback goes in gavins growscript, and Tbubble calls for the player gameobject.

--- Assets/Thomas/Feedbacker/Twening.cs
+++ Assets/Thomas/Feedbacker/Twening.cs
@@ -24,19 +24,27 @@ public class Twening : MonoBehaviour
 
     public GameObject bigBoy;
     public GameObject shooty;
+    public GameObject tree;
+    public GameObject house;
+    public GameObject rok;
+    public GameObject door;
+
     void Start()
     {
         
         transform.localScale = Vector2.zero;
         bigBoy.SetActive(false);
         shooty.SetActive(false);
+        tree.SetActive(false);
+        house.SetActive(false);
+        rok.SetActive(false);
+        door.SetActive(false);
     }
 
     private void Update()
     {
         if (GS.currentSize >= shootReq & consumeShoot == false)
         {
-            Debug.Log("I AM RUNININGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG");
             StartCoroutine(Bubbletwo());
             consumeShoot = true;
         }
@@ -47,9 +55,26 @@ public class Twening : MonoBehaviour
             StartCoroutine(BubbleOne());
             consumeBig = true;
         }
-
-        
-
+        if (GS.currentSize >= treeReq & consumeVeggies == false)
+        {
+            StartCoroutine(Bubblethree());
+            consumeVeggies = true;
+        }
+        if (GS.currentSize >= houseReq & consumeHouse == false)
+        {
+            StartCoroutine(Bubblefour());
+            consumeHouse = true;
+        }
+        if (GS.currentSize >= rokcReq & consumeRcok == false)
+        {
+            StartCoroutine(Bubblefive());
+            consumeRcok = true;
+        }
+        if (GS.currentSize >= doorReq & consumeDoor == false)
+        {
+            StartCoroutine(Bubblesix());
+            consumeDoor = true;
+        }
     }
 
     IEnumerator BubbleOne()
@@ -68,5 +93,36 @@ public class Twening : MonoBehaviour
         transform.LeanScale(Vector2.zero, 0.8f).setEaseOutCubic();
         shooty.SetActive(false);
     }
-
+    IEnumerator Bubblethree()
+    {
+        tree.SetActive(true);
+        transform.LeanScale(new Vector2(1.5f, 1.5f), 0.8f).setEaseOutCubic();
+        yield return new WaitForSeconds(waitingTime);
+        transform.LeanScale(Vector2.zero, 0.8f).setEaseOutCubic();
+        tree.SetActive(false);
+    }
+    IEnumerator Bubblefour()
+    {
+        house.SetActive(true);
+        transform.LeanScale(new Vector2(1.5f, 1.5f), 0.8f).setEaseOutCubic();
+        yield return new WaitForSeconds(waitingTime);
+        transform.LeanScale(Vector2.zero, 0.8f).setEaseOutCubic();
+        house.SetActive(false);
+    }
+    IEnumerator Bubblefive()
+    {
+        rok.SetActive(true);
+        transform.LeanScale(new Vector2(1.5f, 1.5f), 0.8f).setEaseOutCubic();
+        yield return new WaitForSeconds(waitingTime);
+        transform.LeanScale(Vector2.zero, 0.8f).setEaseOutCubic();
+        rok.SetActive(false);
+    }
+    IEnumerator Bubblesix()
+    {
+        door.SetActive(true);
+        transform.LeanScale(new Vector2(1.5f, 1.5f), 0.8f).setEaseOutCubic();
+        yield return new WaitForSeconds(waitingTime);
+        transform.LeanScale(Vector2.zero, 0.8f).setEaseOutCubic();
+        door.SetActive(false);
+    }
 }

65b36d976a802a0e3bfd2735b98dfdf4f1e9eac6
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2022-01-11 08:36:11 -0800

    Fixed stuffs and added starting dialgoue

---
+++ Assets/Matve/Dialogue Scripts/DialogueManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public bool animActive;
+    float timer;
+    float time = 0.6f;
+
+    public GameObject diaCurrentTrigger;
+    GameObject player;
+    PlayerMovement PM;
+    Animator playerAnim;
+
+    public Animator anim;
+
+    private Queue<string> sentences;
+    private Queue<string> names;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        PM = player.GetComponent<PlayerMovement>();
+
+        sentences = new Queue<string>();
+        names = new Queue<string>();
+    }
+
+    private void Update()
+    {
+        if (animActive)
+        {
+            if (time >= timer)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                Time.timeScale = 0;
+            }
+        }
+        else
+        {
+            timer = 0;
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        anim.SetBool("isActive", true);
+        animActive = true;
+        
+        names.Clear();
+        sentences.Clear();
+        
+        foreach (string name in dialogue.name)
+        {
+            names.Enqueue(name);
+        }
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        
+        DisplayNextSentence();
+    }
+
+    
+
+    public void DisplayNextSentence()
+    {
+        
+        if(sentences.Count == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                EndDialogue();
+                return;
+            }
+        }
+        string name = names.Dequeue();
+        string sentence = sentences.Dequeue();
+        nameText.text = name;
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        PM.enabled = true;
+
+        animActive = false;
+        Time.timeScale = 1;
+        Destroy(diaCurrentTrigger);
+        Debug.Log("End of conversation.");
+        anim.SetBool("isActive", false);
+    }
+}
---
+++ Assets/Matve/Dialogue Scripts/StartingDia.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingDia : MonoBehaviour
+{
+    public TriggerTutorial TT;
+
+    public GameObject tutorialtext1;
+    public float timer;
+    float time = 0.1f;
+
+
+    public GameObject text;
+    public GameObject blackScreen;
+    GameObject player;
+    PlayerMovement PM;
+
+    Animator anim;
+
+    int clicks = 4;
+
+    private void Start()
+    {
+        
+        player = GameObject.Find("Player");
+        PM = player.GetComponent<PlayerMovement>();
+        PM.enabled = false;
+        anim = blackScreen.GetComponent<Animator>();
+
+        text.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            clicks -= 1;
+            if(clicks <= 0)
+            {
+                anim.SetBool("begin", true);
+                text.SetActive(true);
+                
+                
+            }
+
+        }
+    }
+
+    private void OnDestroy()
+    {
+        TT.started = true;
+    }
+}

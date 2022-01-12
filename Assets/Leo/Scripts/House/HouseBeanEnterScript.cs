c8a54cee13c411fc0338024cb6bc21f2d4f29054
Leo <leo_1517@outlook.com>
2022-01-12 10:22:03 -0800

    Finished houses with Jav

---
+++ Assets/Leo/Scripts/House/HouseBeanEnterScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseBeanEnterScript : MonoBehaviour
+{
+    HouseScript houseScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        houseScript = transform.parent.GetComponent<HouseScript>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        houseScript.BeanEnter(collision);
+    }
+}

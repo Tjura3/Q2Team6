71737a72b9e5a5cd4945c7ed7047f4e736fe5685
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2022-01-12 09:31:32 -0800

    I FIXED THINGS

---
+++ Assets/Matve/Scripts/Dialogue Scripts/TriggerTutorial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTutorial : MonoBehaviour
+{
+    public GameObject tutorial;
+
+    public bool started;
+
+    public float time;
+    float timer;
+    void Start()
+    {
+        tutorial.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started)
+        {
+            if(time >= timer)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                tutorial.SetActive(true);
+            }
+        }
+    }
+}

68435d0e0a0cb331e3c6a773141316d5a84e2d68
Thomas <1707235@mukilteo.wednet.edu>
2022-01-12 16:17:33 -0800

    no message

--- Assets/Thomas/DeathScene/DeathOut.cs
+++ Assets/Thomas/DeathScene/DeathOut.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class DeathOut : MonoBehaviour
 {
 
-    public GameObject transit;
+    
     
     void Start()
     {
         transform.localScale = Vector2.zero;
-        
     }
 
     // Update is called once per frame
@@ -24,7 +23,7 @@ public class DeathOut : MonoBehaviour
     IEnumerator BubbleOne()
     {
 
-        transform.LeanScale(new Vector2(1.5f, 1.5f), 0.8f).setEaseOutCubic();
+        transform.LeanScale(Vector2.one, 0.8f).setEaseOutCubic();
         yield return new WaitForSeconds(2f);
         transform.LeanScale(Vector2.zero, 0.8f).setEaseOutCubic();
 

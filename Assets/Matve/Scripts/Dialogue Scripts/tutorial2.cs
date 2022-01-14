91b35bdf601a5dde0f859df393a7e01529c4f03b
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2022-01-14 08:53:30 -0800

    Tutorial finished buh without transition

--- Assets/Matve/Scripts/Dialogue Scripts/tutorial2.cs
+++ Assets/Matve/Scripts/Dialogue Scripts/tutorial2.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class tutorial2 : MonoBehaviour
 {
+    SizeTrigger ST;
+
     public GameObject tongue;
     // Start is called before the first frame update
     void Start()

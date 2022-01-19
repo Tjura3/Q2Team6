bc0658ad0c2c7f400818f8782396f48ab6925302
MatthewRacela <1707226@mukilteo.wednet.edu>
2022-01-18 20:25:47 -0800

    Da poosh

---
+++ Assets/Matve/Scripts/Dialogue Scripts/Scripts/Combat/InstaDie.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstaDie : MonoBehaviour
+{
+    healthSystem HS;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        HS = player.GetComponent<healthSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            HS.healthPoints = 0;
+        }
+    }
+}

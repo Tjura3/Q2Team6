05f36160f07c6954e2a8a73433500eb1819f8646
Gavin <gavinjohnson2004@gmail.com>
2022-01-18 19:54:29 -0800

    Death stuff fixed

---
+++ Assets/Gavin/Scripts/PreviousScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public static class PreviousScene
+{
+    public static int previousSceneBuildIndex;
+}

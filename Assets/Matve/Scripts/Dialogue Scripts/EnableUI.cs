3a225144dccba9a455e035bc15d8ea914a162ea4
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2022-01-19 08:50:15 -0800

    Did som tutorial stuff

---
+++ Assets/Matve/Scripts/Dialogue Scripts/EnableUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableUI : MonoBehaviour
+{
+    public GameObject UI1;
+    public GameObject UI2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI1.SetActive(false);
+        UI2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            UI1.SetActive(true);
+            UI2.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Matve/Scripts/Dialogue Scripts/Scripts/RUN2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RUN2 : MonoBehaviour
+{
+    SizeTrigger ST;
+    public GameObject barrier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ST = GetComponent<SizeTrigger>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ST.activated)
+        {
+            Destroy(barrier);
+        }
+    }
+}
